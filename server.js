5ca46678887dffdb126c5442e28c526858c0aa99
Christopher Young <christopheryoung@MacBook123.local>
2022-10-17 22:42:00 -0700

    removed previous changes

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const helmet = require("helmet");
-const path = require("node:path");
 
 //----- Middleware
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(express.static(path.join(__dirname, "client/build")));
 
 //----- Routes
 app.use(require("./routes/imageSearch"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
  
 //----- Connection
 app.listen(port, () => {
